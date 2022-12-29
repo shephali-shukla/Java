54d204620d2353f07f22dcd67b1883e75cf805f0
Shephali Shukla <shephalishukla1@gmail.com>
2022-12-29 16:28:30 +0530

    First String code

---
+++ 02-Strings/Basic.java
@@ -0,0 +1,8 @@
+//Note: String is represented inside "".
+public class Basic {
+    public static void main(String[] args) {
+        
+        String str = "Hello World";
+        System.out.println(str);
+    }
+}
