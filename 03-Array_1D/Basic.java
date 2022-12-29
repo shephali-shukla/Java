288d52ee24167f9488fd7c13ab08396c171a5990
Shephali Shukla <shephalishukla1@gmail.com>
2022-12-29 16:29:32 +0530

    Array Declaration & Initialization

---
+++ 03-Array_1D/Basic.java
@@ -0,0 +1,27 @@
+//Code: Declaration & Initialization of Array.
+//Important: Apart form primitive data type. Arrays of Object can be made as well.
+
+/*
+ * Note: Declaration: 
+ * a) dataType arrayName[];
+ * b) dataType[] arrayName; (Preferred for Java)
+ */
+
+ /*
+  * Note: Initializtion: It gives memory to array elements.
+  * arrayName = new dataType[size]
+  */
+
+  /*Note: Declaring & Initializing in one line:
+   * int[] arr = new int[5];
+   * 
+   * Important: Hardcoding the value: int[] arr = {1,2,3,4,5};
+   */
+
+public class Basic{
+    public static void main(String[] args) {
+        
+        int[] arr = {5,4,3,2,1};
+        String[] str = {"Hello", "World"};
+    }
+}
