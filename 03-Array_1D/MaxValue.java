261fc6c9a8bccf6ae1d10c88022de176a175d0cd
Shephali Shukla <shephalishukla1@gmail.com>
2023-01-04 11:05:42 +0530

    Max value in an array

--- 03-Array_1D/MaxValue.java
+++ 03-Array_1D/MaxValue.java
@@ -1,26 +1,23 @@
-//Program to find minimum value in the array.
-/*Note: Time Complexity- O(n) & Space Complexity- O(1) */
+//To find maximum element in the array.
+//This is a brute approach with Time Complexity: O(n) & Space Complexity: O(1)
 public class MaxValue {
     
-    public int minElement(int[] arr){
-
-        //edge case
-        if(arr == null || arr.length == 0){
+    public int maxValue(int[] num){
+        //edge case:
+        if(num.length == 0 || num == null){
             throw new IllegalArgumentException("Invalid Input");
         }
-        int min = arr[0];
-        for(int i=1;i<arr.length;i++){
-            if(arr[i] < min){
-                min = arr[i];
+        int max = num[0];
+        for(int i=0;i<num.length;i++){
+            if(num[i] > max){
+                max = num[i];
             }
         }
-        return min;
+        return max;
     }
 
     public static void main(String[] args) {
-        int arr[] = {5,8,44,1,6};
         MaxValue mx = new MaxValue();
-        System.out.println("Minimum value in array: " + mx.minElement(arr));
-        //System.out.println(mx.minElement(new int[0]));
+        System.out.println("The max value is: " + mx.maxValue(new int[]{5,7,9,6,12,11}));
     }
 }
