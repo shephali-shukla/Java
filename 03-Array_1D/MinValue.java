a4f066e229bb4effa6aafa1c62a85be1a9cc1b0a
Shephali Shukla <shephalishukla1@gmail.com>
2023-01-04 11:05:57 +0530

    Min value in an array

---
+++ 03-Array_1D/MinValue.java
@@ -0,0 +1,26 @@
+//Program to find minimum value in the array.
+/*Note: Time Complexity- O(n) & Space Complexity- O(1) */
+public class MinValue {
+    
+    public int minElement(int[] arr){
+
+        //edge case
+        if(arr == null || arr.length == 0){
+            throw new IllegalArgumentException("Invalid Input");
+        }
+        int min = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,8,44,1,6};
+        MinValue mi = new MinValue();
+        System.out.println("Minimum value in array: " + mi.minElement(arr));
+        //System.out.println(mx.minElement(new int[0]));
+    }
+}
